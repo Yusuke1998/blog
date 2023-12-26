91afa810e6ddb26209c44c87032d8130942d91cd
yusuke1998 <jhonnyjose1998@gmail.com>
2023-12-26 12:59:02 -0400

    [ADD] Ajustes varios
    - Se agrega logica para mostrar el contenido/descripcion de cada pagina.
    - Nuevo componente para paginacion con vue.

--- frontend/src/env.d.ts
+++ frontend/src/env.d.ts
@@ -1,5 +1,8 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 interface ImportMetaEnv {
-    readonly STRAPI_URL: string;
+    readonly PUBLIC_STRAPI_URL: string;
+    readonly PUBLIC_STRAPI_ASSETS_URL: string;
+    readonly PUBLIC_DEFAULT_IMAGE: string;
+    readonly PUBLIC_AUTHOR: string;
 }
