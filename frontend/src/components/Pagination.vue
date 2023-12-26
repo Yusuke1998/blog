5a0ada308a0676cfd0154c5272da1e97c562091f
yusuke1998 <jhonnyjose1998@gmail.com>
2023-12-26 12:59:02 -0400

    [ADD] Ajustes varios
    - Se agrega logica para mostrar el contenido/descripcion de cada pagina.
    - Nuevo componente para paginacion con vue.

---
+++ frontend/src/components/Pagination.vue
@@ -0,0 +1,86 @@
+<script>
+export default {
+  name: "Pagination",
+  props: {
+    total: Number,
+    perPage: Number,
+    page: Number,
+    totalPages: Number
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    prevPage() {
+      this.$emit('prevPage', this.page - 1)
+    },
+    nextPage() {
+      this.$emit("nextPage", this.page + 1);
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="flex flex-col items-center">
+    <!-- Help text -->
+    <span class="text-sm text-gray-700 dark:text-gray-400">
+      Mostrando
+      <span class="font-semibold text-gray-900 dark:text-white">{{
+        page
+      }}</span>
+      de
+      <span class="font-semibold text-gray-900 dark:text-white">{{
+        totalPages
+      }}</span>
+      Paginas
+    </span>
+    <div class="inline-flex mt-2 xs:mt-0">
+      <!-- Buttons -->
+      <button
+        @click="prevPage"
+        class="flex items-center justify-center px-3 h-8 text-sm font-medium text-white bg-gray-800 rounded-s hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+        :class="(page <= 1) ? 'pointer-events-none opacity-50' : ''"
+      >
+        <svg
+          class="w-3.5 h-3.5 me-2 rtl:rotate-180"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 14 10"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M13 5H1m0 0 4 4M1 5l4-4"
+          />
+        </svg>
+        Ant
+      </button>
+      <button
+        @click="nextPage"
+        class="flex items-center justify-center px-3 h-8 text-sm font-medium text-white bg-gray-800 border-0 border-s border-gray-700 rounded-e hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+        :class="(!(totalPages > 1) || page == totalPages) ? 'pointer-events-none opacity-50' : ''"
+      >
+        Sig
+        <svg
+          class="w-3.5 h-3.5 ms-2 rtl:rotate-180"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 14 10"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M1 5h12m0 0L9 1m4 4L9 9"
+          />
+        </svg>
+      </button>
+    </div>
+  </div>
+</template>
--- frontend/src/lib/strapi.ts
+++ frontend/src/lib/strapi.ts
@@ -9,7 +9,7 @@ interface Props {
  * Obtiene datos de la API de Strapi.
  * @param endpoint - El endpoint para realizar la consulta
  * @param query - Los parámetros de consulta para agregar a la URL
- * @param wrappedByKey - La clave para desempaquetar la respuesta
+ * @param wrappedByKey - La clave para desempaquetar la respuesta (Pueden ser n separada por coma)
  * @param wrappedByList - Si la respuesta es una lista, desempaquétala.
  * @returns
  */
@@ -22,18 +22,28 @@ export default async function fetchApi<T>({
     if (endpoint.startsWith('/')) {
         endpoint = endpoint.slice(1);
     }
-    const apiHost = import.meta.env.STRAPI_URL || 'http://127.0.0.1:1337'
+    const apiHost = import.meta.env.PUBLIC_STRAPI_URL || 'http://127.0.0.1:1337'
     const url = new URL(`${apiHost}/api/${endpoint}`);
     if (query) {
         Object.entries(query).forEach(([key, value]) => {
             url.searchParams.append(key, value);
         });
     }
+    console.log(url.toString())
     const res = await fetch(url.toString());
     let data = await res.json();
 
     if (wrappedByKey) {
-        data = data[wrappedByKey];
+        if (wrappedByKey.match(',')) {
+            const arrSplit = wrappedByKey.split(',');
+            let newData: any = {};
+            for (let key of arrSplit) {
+                newData[key] = data[key];
+            }
+            data = newData;
+        } else {
+            data = data[wrappedByKey];
+        }
     }
 
     if (wrappedByList) {
